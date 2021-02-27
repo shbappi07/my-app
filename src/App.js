e4566ecd12b5c34b2f072cc2d021ff76867ce807
shbappi07 <you@example.comshbappi07@gmail.com>
2021-02-28 02:11:41 +0600

    create component dynamic

--- src/App.js
+++ src/App.js
@@ -2,22 +2,65 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const productsLists = [
+    {name: "Adobe", price: "$99.99"},
+    {name: "Illustrator", price: "$89.99"},
+    {name: "Adobe Premier", price: "$199.99"}
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Products name = {productsLists[0].name} price = {productsLists[0].price}></Products>
+        <Products name = {productsLists[1].name} price = {productsLists[0].price}></Products>
+        <Products name = {productsLists[2].name} price = {productsLists[0].price}></Products>
+        <Person name = "Salimul" prof = "student"></Person>
+        <Person name = "korimul" prof = "Bekar"></Person>
+        <Person name = "alimul" prof = "sakar"></Person>
+        <Person name = "khan" prof = "Denadar"></Person>
       </header>
+    </div>
+  );
+}
+
+function Person(props) {
+
+  const personStyle = {
+    
+    border:'1px solid green',
+    padding: '5px',
+    margin: '10px'
+  }
+
+  return (
+    <div style = {personStyle}> 
+
+      <h1>Welcome to react</h1>
+      <h2>{props.name}</h2>
+      <h3>Profession:{" "}{props.prof} </h3>
+
+    </div>
+    
+    
+  );
+}
+// products practices
+function Products(props) {
+  const productsStyle = {
+    border: '2px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '400px',
+    float: 'left',
+    color: 'black'
+  }
+  return(
+    <div style = {productsStyle}>
+      <h2>Name: {props.name}</h2>
+      <h3>Price: {props.price}</h3>
+      <button>Buy Now</button>
     </div>
   );
 }
